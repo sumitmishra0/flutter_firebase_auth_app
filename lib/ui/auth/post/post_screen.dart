d4452b26815205e0482f7c54948750b42351b775
sumit mishra <mishra.sumit@thinksys.com>
2023-09-01 18:38:56 +0530

    used stream builder for showing the list of data and added filtering options

--- lib/ui/auth/post/post_screen.dart
+++ lib/ui/auth/post/post_screen.dart
@@ -16,6 +16,8 @@ class PostScreen extends StatefulWidget {
 class _PostScreenState extends State<PostScreen> {
   final _auth = FirebaseAuth.instance;
   final ref = FirebaseDatabase.instance.ref('Post');
+  final searchController = TextEditingController();
+
   Future<void> signOut(context) async {
     try {
       final value = await _auth.signOut();
@@ -34,6 +36,7 @@ class _PostScreenState extends State<PostScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Post"),
+        automaticallyImplyLeading: false,
         actions: [
           IconButton(
               onPressed: () async => signOut(context),
@@ -52,14 +55,70 @@ class _PostScreenState extends State<PostScreen> {
       ),
       body: Column(
         children: [
+          SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: TextFormField(
+              controller: searchController,
+              onChanged: (String value) {
+                setState(() {});
+              },
+              decoration: InputDecoration(
+                  hintText: 'Search', border: OutlineInputBorder()),
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          // Stream builder
+
+          // Expanded(child: StreamBuilder(
+          //   stream: ref.onValue,
+          //   builder: (context, AsyncSnapshot<DatabaseEvent> snapshot){
+          //   if(!snapshot.hasData){
+          //     return CircularProgressIndicator();
+          //   } else {
+
+          //     Map<dynamic , dynamic> map = snapshot.data!.snapshot.value as dynamic;
+          //     List<dynamic> list = [];
+          //     list.clear();
+          //     list = map.values.toList();
+
+          //     return ListView.builder(
+          //       itemCount: snapshot.data!.snapshot.children.length,
+          //       itemBuilder: (context, index){
+          //       return ListTile(
+          //         title: Text(list[index]['note']),
+          //         subtitle: Text(list[index]['id'].toString()),
+          //       );
+          //     });
+          //   }
+          // })),
+
+          // Firebase Animated Widget
           Expanded(
             child: FirebaseAnimatedList(
               query: ref,
               itemBuilder: (context, snapshot, animation, index) {
-                return ListTile(
-                  title: Text(snapshot.child('note').value.toString()),
-                  subtitle: Text(snapshot.child('id').value.toString()),
-                );
+                final title = snapshot.child('note').value.toString();
+
+                if (searchController.text.isEmpty) {
+                  return ListTile(
+                    title: Text(snapshot.child('note').value.toString()),
+                    subtitle: Text(snapshot.child('id').value.toString()),
+                  );
+                } else if (title
+                    .toLowerCase()
+                    .contains(searchController.text.toLowerCase().toString())) {
+                  return ListTile(
+                    title: Text(snapshot.child('note').value.toString()),
+                    subtitle: Text(snapshot.child('id').value.toString()),
+                  );
+                } else {
+                  return Container();
+                }
               },
               defaultChild: Text("Loading"),
             ),

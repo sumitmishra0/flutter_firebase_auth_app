1a206295aa16c36ce21fbee841407122460fcf22
sumit mishra <mishra.sumit@thinksys.com>
2023-09-01 17:45:47 +0530

    saved and read data in firebase realtime database

--- lib/ui/auth/post/post_screen.dart
+++ lib/ui/auth/post/post_screen.dart
@@ -1,6 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/ui/auth/login_screen.dart';
+import 'package:flutter_application_1/ui/auth/post/add_posts.dart';
 import 'package:flutter_application_1/utils/utils.dart';
 
 class PostScreen extends StatefulWidget {
@@ -12,7 +15,7 @@ class PostScreen extends StatefulWidget {
 
 class _PostScreenState extends State<PostScreen> {
   final _auth = FirebaseAuth.instance;
-
+  final ref = FirebaseDatabase.instance.ref('Post');
   Future<void> signOut(context) async {
     try {
       final value = await _auth.signOut();
@@ -32,13 +35,37 @@ class _PostScreenState extends State<PostScreen> {
       appBar: AppBar(
         title: Text("Post"),
         actions: [
-          IconButton(onPressed: () async => signOut(context), icon: Icon(Icons.login_outlined)),
+          IconButton(
+              onPressed: () async => signOut(context),
+              icon: Icon(Icons.login_outlined)),
           SizedBox(
             width: 10,
           ),
         ],
       ),
-      body: Column(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => AddPostScreen()));
+        },
+        child: Icon(Icons.add),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: FirebaseAnimatedList(
+              query: ref,
+              itemBuilder: (context, snapshot, animation, index) {
+                return ListTile(
+                  title: Text(snapshot.child('note').value.toString()),
+                  subtitle: Text(snapshot.child('id').value.toString()),
+                );
+              },
+              defaultChild: Text("Loading"),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
